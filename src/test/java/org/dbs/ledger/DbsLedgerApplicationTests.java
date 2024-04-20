cf3907e2a75e827112b3643322d89f0e3ff4cd3e
DevilsZone <akash@grayfrost.app>
2024-04-20 13:03:39 +0530

    Base Commit

---
+++ src/test/java/org/dbs/ledger/DbsLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dbs.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
