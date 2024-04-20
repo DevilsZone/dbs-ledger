5123db8442d8d0487ab4283324eb302f6921fdd7
DevilsZone <akash@grayfrost.app>
2024-04-20 15:55:46 +0530

    Service and Global Error Handling

--- src/main/java/org/dbs/ledger/enums/ErrorCode.java
+++ src/main/java/org/dbs/ledger/enums/ErrorCode.java
@@ -7,27 +7,16 @@ import org.dbs.ledger.util.Constants;
 @Getter
 @ToString
 public enum ErrorCode {
+
     INTERNAL_SERVER_ERROR("001", "Internal Server Error"),
     INPUT_VALIDATION_ERROR("002", "Input Validation Error"),
     INVALID_JWT_TOKEN("003", "Invalid JWT token provided"),
     JWT_TOKEN_EXPIRED("004", "JWT token expired"),
     TOKEN_MUST_START_WITH_BEARER("005", "Token must start with Bearer"),
-    DUPLICATE_ORGANIZATION("006", "Organization already exist"),
-    NOT_FOUND("007", "Not Found"),
-    DUPLICATE_TEACHER("008", "Teacher already exist"),
-    STAFF_ALREADY_EXISTS("009", "Staff already exist"),
-    DUPLICATE_BRANCH("010", "Branch already exist"),
-    DUPLICATE_INFRASTRUCTURE_SPACE("011", "Infrastructure space already exist"),
-    DUPLICATE_ACCOUNT("012", "Account already exists"),
-    DUPLICATE_ADMIN("013", "Admin already exists"),
-    DUPLICATE_PARENT("014", "Parent already exists"),
-    DUPLICATE_FEE_HEAD("015", "Fee head already exists"),
-    DUPLICATE_FEE_STRUCTURE("016", "Fee Structure already exists"),
-    DUPLICATE_PROGRAM("017", "Program already exists"),
-    DUPLICATE_COURSE("018", "Course already exist"),
-    DUPLICATE_COURSE_SESSION("019", "Course Session already exists"),
-    ORGANIZATION_ACCOUNT_NOT_LINKED("020", "Organization is not owned by the account"),
-    DUPLICATE_PROGRAM_SESSION("021", "Program Session already exists");
+    ACCOUNT_NOT_FOUND("006", "Account not found for user"),
+    BALANCE_NOT_AVAILABLE("007", "Required Balance Not Available"),
+    TRANSFER_FAILED("008", "Account entry creation failed")
+    ;
 
     private final String code;
 
--- src/main/java/org/dbs/ledger/service/impl/TransactionServiceImpl.java
+++ src/main/java/org/dbs/ledger/service/impl/TransactionServiceImpl.java
@@ -2,9 +2,12 @@ package org.dbs.ledger.service.impl;
 
 import org.dbs.ledger.dto.request.TransactionRequest;
 import org.dbs.ledger.dto.response.TransactionResponse;
+import org.dbs.ledger.dto.response.wrapper.ErrorResponse;
 import org.dbs.ledger.enums.AccountBalanceOutputStatus;
 import org.dbs.ledger.enums.AccountEntryOutputStatus;
+import org.dbs.ledger.enums.ErrorCode;
 import org.dbs.ledger.enums.TransactionStatus;
+import org.dbs.ledger.exceptions.RestException;
 import org.dbs.ledger.helper.AccountEntryHelper;
 import org.dbs.ledger.helper.AccountHelper;
 import org.dbs.ledger.model.output.AccountBalanceOutput;
@@ -12,6 +15,7 @@ import org.dbs.ledger.model.output.AccountEntryOutput;
 import org.dbs.ledger.service.TransactionService;
 import org.dbs.ledger.transformer.TransactionTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,23 +40,23 @@ public class TransactionServiceImpl implements TransactionService {
         AccountBalanceOutput fromAccountBalance = accountHelper.getAccountBalance(transactionRequest.getFromAccountId());
         AccountBalanceOutput toAccountBalance = accountHelper.getAccountBalance(transactionRequest.getToAccountId());
         if (!fromAccountBalance.accountBalanceOutputStatus().equals(AccountBalanceOutputStatus.SUCCESS)) {
-            throw new RuntimeException();
+            throw new RestException(HttpStatus.UNAUTHORIZED, ErrorResponse.from(ErrorCode.ACCOUNT_NOT_FOUND));
         }
         if (!toAccountBalance.accountBalanceOutputStatus().equals(AccountBalanceOutputStatus.SUCCESS)) {
-            throw new RuntimeException();
+            throw new RestException(HttpStatus.UNAUTHORIZED, ErrorResponse.from(ErrorCode.ACCOUNT_NOT_FOUND));
         }
         if (fromAccountBalance.accountBalance().availableBalance() < transactionRequest.getAmount()) {
-            throw new RuntimeException();
+            throw new RestException(HttpStatus.BAD_REQUEST, ErrorResponse.from(ErrorCode.BALANCE_NOT_AVAILABLE));
         }
 
         AccountBalanceOutput accountBalanceOutput = accountHelper.updateAccountBalance(transactionTransformer.convertTransactionRequestToAccountInput(transactionRequest));
         if (!accountBalanceOutput.accountBalanceOutputStatus().equals(AccountBalanceOutputStatus.SUCCESS)) {
-            throw new RuntimeException();
+            throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, ErrorResponse.from(ErrorCode.INTERNAL_SERVER_ERROR));
         }
 
         AccountEntryOutput accountEntry = accountEntryHelper.createAccountEntry(transactionTransformer.convertTransactionRequestToEntryInput(transactionRequest));
         if (!accountEntry.accountEntryOutputStatus().equals(AccountEntryOutputStatus.SUCCESS)) {
-            throw new RuntimeException();
+            throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, ErrorResponse.from(ErrorCode.INTERNAL_SERVER_ERROR));
         }
 
 
