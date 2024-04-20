e3b1613b10807cd8108fe79587947d35833d47d2
DevilsZone <akash@grayfrost.app>
2024-04-20 14:45:41 +0530

    Set up Impl for helper

---
+++ src/main/java/org/dbs/ledger/helper/impl/AccountEntryHelperImpl.java
@@ -0,0 +1,14 @@
+package org.dbs.ledger.helper.impl;
+
+import org.dbs.ledger.annotation.Helper;
+import org.dbs.ledger.helper.AccountEntryHelper;
+import org.dbs.ledger.model.input.AccountEntryInput;
+import org.dbs.ledger.model.output.AccountEntryOutput;
+
+@Helper
+public class AccountEntryHelperImpl implements AccountEntryHelper {
+    @Override
+    public AccountEntryOutput createAccountEntry(AccountEntryInput accountEntryInput) {
+        return null;
+    }
+}
---
+++ src/main/java/org/dbs/ledger/helper/impl/AccountHelperImpl.java
@@ -0,0 +1,13 @@
+package org.dbs.ledger.helper.impl;
+
+import org.dbs.ledger.annotation.Helper;
+import org.dbs.ledger.helper.AccountHelper;
+import org.dbs.ledger.model.input.AccountBalanceUpdateInput;
+
+@Helper
+public class AccountHelperImpl implements AccountHelper {
+    @Override
+    public void updateAccountBalance(AccountBalanceUpdateInput accountBalanceUpdateInput) {
+
+    }
+}
