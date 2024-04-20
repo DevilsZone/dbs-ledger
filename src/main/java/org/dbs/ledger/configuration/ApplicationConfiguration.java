05582f3b10e6ab339a84adb2cdef4098edd3ea7e
DevilsZone <akash@grayfrost.app>
2024-04-20 14:30:04 +0530

    Transactional Bean

--- src/main/java/org/dbs/ledger/configuration/ApplicationConfiguration.java
+++ src/main/java/org/dbs/ledger/configuration/ApplicationConfiguration.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 
 @Configuration
 @EnableWebMvc
@@ -19,5 +21,10 @@ public class ApplicationConfiguration {
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                 .findAndAddModules()
                 .build();
+    }
+
+    @Bean
+    MongoTransactionManager transactionManager(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTransactionManager(mongoDatabaseFactory);
     }
 }
---
+++ src/main/java/org/dbs/ledger/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,8 @@
+package org.dbs.ledger.service.impl;
+
+import org.dbs.ledger.service.TransactionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+}
